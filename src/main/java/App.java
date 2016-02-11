1d20fb9eaccb07bfc26f91860922ba9779144e90
10igeorge <10izzygeorge@gmail.com>
2016-02-11 13:41:48 -0800

    create first passing test to replace the letter a with a dash

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,23 @@
+import java.util.Map;
 import java.util.HashMap;
-
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
-
 import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
+  }
 
-
+  public static String replaceVowel(String word) {
+    String finalWord = "";
+    char[] splitWord = word.toCharArray();
+    for (Integer i=0; i < splitWord.length; i++){
+      if (splitWord[i] == ('a')){
+        finalWord = finalWord + "-";
+      } else {
+        finalWord = finalWord + splitWord[i];
+      }
+    }
+    return finalWord;
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,4 +2,10 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class AppTest {
+
+  @Test
+  public void replaceVowel_replacesVowelAWithDash_replaceA(){
+    App replaceA = new App();
+     assertEquals("--rdv-rk", replaceA.replaceVowel("aardvark"));
+  }
 }
