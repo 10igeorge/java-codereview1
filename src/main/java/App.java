ce34e449e5d617dd550222b25d203cb9df79b649
10igeorge <10izzygeorge@gmail.com>
2016-02-11 14:21:09 -0800

    create running home page in spark

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,6 +6,14 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+    
   }
 
   public static String replaceVowel(String word) {
