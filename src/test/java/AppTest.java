23d9aa2d11e82fe65ab37ad9f936124e0381999b
10igeorge <10izzygeorge@gmail.com>
2016-02-11 14:06:22 -0800

    refactor entire junit to onlye one test that replaces all vowels with dashes

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -3,31 +3,7 @@ import static org.junit.Assert.*;
 
 public class AppTest {
 
-  @Test
-  public void replaceVowel_replacesVowelAWithDash_replaceA(){
-    App replaceA = new App();
-     assertEquals("-", replaceA.replaceVowel("a"));
-  }
-
-  @Test
-  public void replaceVowel_replacesVowelAEWithDash_replaceE(){
-    App replaceE = new App();
-     assertEquals("-", replaceE.replaceVowel("e"));
-  }
-
-  @Test
-  public void replaceVowel_replacesVowelAEIWithDash_replaceI(){
-    App replaceI = new App();
-     assertEquals("-", replaceI.replaceVowel("i"));
-  }
-
-  @Test
-  public void replaceVowel_replacesVowelAEIOWithDash_replaceO(){
-    App replaceO = new App();
-     assertEquals("-", replaceO.replaceVowel("o"));
-  }
-
-  @Test
+@Test
   public void replaceVowel_replacesVowelAEIOUWithDash_replaceU(){
     App replaceU = new App();
      assertEquals("-mb-d-xtr--s", replaceU.replaceVowel("ambidextrous"));
