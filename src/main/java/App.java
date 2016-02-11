42972be863856444df4550b8099ca0b7241dbf8e
10igeorge <10izzygeorge@gmail.com>
2016-02-11 14:31:34 -0800

    ensure that submit button redirects to the puzzle results page in spark

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,13 +7,24 @@ import static spark.Spark.*;
 public class App {
   public static void main(String[] args) {
     String layout = "templates/layout.vtl";
+    staticFileLocation("/public");
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/home.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
-    
+
+    get("/puzzle", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/puzzle.vtl");
+
+      String userInput = request.queryParams("userInput");
+      String puzzle = replaceVowel(userInput);
+      model.put("puzzle", puzzle);
+
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 
   public static String replaceVowel(String word) {
